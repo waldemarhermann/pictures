b22402640af6c26515df366d0b8917b642585557
waldemarhermann <hermann.waldemar1995@gmail.com>
2023-03-09 22:27:23 +0100

    add component SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,24 @@
-const SearchBar = () => {
-    return (
-        <div></div>
-    )
+import { useState } from 'react';
+
+const SearchBar = ({ onSubmit }) => {
+  const [term, setTerm] = useState('');
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    onSubmit();
+  };
+
+const handleChange = (event) => {
+    setTerm(event.target.value);
+}
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input value={term} onChange={handleChange} />
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
