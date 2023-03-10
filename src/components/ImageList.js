6cd4b0b4520dc5f4c3112b1148bb4388516ced5d
waldemarhermann <hermann.waldemar1995@gmail.com>
2023-03-10 23:22:32 +0100

    implemented new array in ImageShow component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,16 @@
-const ImageList = () => {
-    return (
-        <div>
+import './ImageList.css';
+import ImageShow from './ImageShow';
 
-        </div>
-    )
+const ImageList = ({ images }) => {
+    const renderImages = images.map(image => {
+        return <ImageShow key={image.id} image={image} />
+    })
+
+  return (
+    <div>
+      <div className='image-list'>{renderImages}</div>
+    </div>
+  );
 };
 
 export default ImageList;
